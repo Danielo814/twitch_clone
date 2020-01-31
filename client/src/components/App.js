4cdb0b41b332a0de43fae86db71d362d18df4c93
Danielo814 <ojedadaniel92@gmail.com>
2020-01-30 17:49:19 -0800

    remove semicolons from jsx

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -11,11 +11,11 @@ const App = () => {
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact path={StreamList} />;
-          <Route path="/streams/new" exact path={StreamCreate} />;
-          <Route path="/streams/edit" exact path={StreamEdit} />;
-          <Route path="/streams/delete" exact path={StreamDelete} />;
-          <Route path="/streams/show" exact path={StreamShow} />;
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
