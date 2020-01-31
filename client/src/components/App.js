9a35d98dddf1c15ec775b242441fb50acc8c0f97
Danielo814 <ojedadaniel92@gmail.com>
2020-01-30 17:45:33 -0800

    import components to route react app

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,21 +1,21 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-
-const PageOne = () => {
-  return <div>PageOne</div>;
-};
-
-const PageTwo = () => {
-  return <div>PageTwo</div>;
-};
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pageTwo" component={PageTwo} />
+          <Route path="/" exact path={StreamList} />;
+          <Route path="/streams/new" exact path={StreamCreate} />;
+          <Route path="/streams/edit" exact path={StreamEdit} />;
+          <Route path="/streams/delete" exact path={StreamDelete} />;
+          <Route path="/streams/show" exact path={StreamShow} />;
         </div>
       </BrowserRouter>
     </div>
