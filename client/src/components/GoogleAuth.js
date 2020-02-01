0adb9cf2be75dda5bfdbdbd1e8450c6e5e32e250
Danielo814 <ojedadaniel92@gmail.com>
2020-01-31 22:12:04 -0800

    fit setstate method

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
-  state = { isSignedIn = null };
+  state = { isSignedIn: null };
 
   componentDidMount() {
     window.gapi.load("client: auth2", () => {
@@ -13,7 +13,7 @@ class GoogleAuth extends React.Component {
         })
         .then(() => {
           this.auth = window.gapi.auth2.getAuthInstance();
-          this.setState = this.auth.setState({
+          this.setState({
             isSignedIn: this.auth.isSignedIn.get()
           });
         });
@@ -21,17 +21,17 @@ class GoogleAuth extends React.Component {
   }
 
   renderAuthButton() {
-      if (this.state.isSignedIn === null) {
-          return <div>I don't know if we are signed in.</div>
-      } else if (this.state.isSignedIn) {
-          return <div>I am signed in!</div>
-      } else {
-          return <div>I am not signed in.</div>
-      }
+    if (this.state.isSignedIn === null) {
+      return <div>I don't know if we are signed in.</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in!</div>;
+    } else {
+      return <div>I am not signed in.</div>;
+    }
   }
 
   render() {
-  return <div>{this.renderAuthButton()}</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
