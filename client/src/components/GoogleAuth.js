97ea4eba81e41f4072b607068ed065a698d914f3
Danielo814 <ojedadaniel92@gmail.com>
2020-01-31 20:30:36 -0800

    case for not being signed in

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -25,6 +25,8 @@ class GoogleAuth extends React.Component {
           return <div>I don't know if we are signed in.</div>
       } else if (this.state.isSignedIn) {
           return <div>I am signed in!</div>
+      } else {
+          return <div>I am not signed in.</div>
       }
   }
 
