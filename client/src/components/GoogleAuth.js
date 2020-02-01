729c245db7a688d9cc6c080813700c6b4acbff8e
Danielo814 <ojedadaniel92@gmail.com>
2020-01-31 20:18:01 -0800

    add then to handle promise returned from google auth

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -3,11 +3,13 @@ import React from "react";
 class GoogleAuth extends React.Component {
   componentDidMount() {
     window.gapi.load("client: auth2", () => {
-      window.gapi.client.init({
-        clientId:
-          "604334040053-tci2as0f3h7rngjv58meiq3dm07ka4bq.apps.googleusercontent.com",
-        scope: "email"
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            "604334040053-tci2as0f3h7rngjv58meiq3dm07ka4bq.apps.googleusercontent.com",
+          scope: "email"
+        })
+        .then(() => {});
     });
   }
 
