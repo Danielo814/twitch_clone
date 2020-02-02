9d08b01dfaf6d37fd70963824c6f4d2ebadde13a
Danielo814 <ojedadaniel92@gmail.com>
2020-02-01 16:54:08 -0800

    fix import mistake

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import reducers from "./reducers";
-import { Provider } from "./react-redux";
+import { Provider } from "react-redux";
 import { createStore } from "redux";
 
 const store = createStore(reducers);
