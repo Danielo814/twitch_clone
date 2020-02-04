5576a2833ef29158a7996ef22655f1cb834d1313
Danielo814 <ojedadaniel92@gmail.com>
2020-02-04 14:53:28 -0800

    add onchange property to input

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,8 +2,14 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  renderInput() {
-    return <input />;
+  renderInput(formProps) {
+    console.log(formProps);
+    return (
+      <input
+        onChange={formProps.input.onChange}
+        value={formProps.input.value}
+      />
+    );
   }
 
   render() {
