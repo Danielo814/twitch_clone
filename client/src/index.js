2a0f78ed3cc45f39fb1e0dc80be5eb4afc46abe4
Danielo814 <ojedadaniel92@gmail.com>
2020-02-03 18:33:01 -0800

    add library

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import reducers from "./reducers";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
+const composeEnhancers = window.__redux;
 const store = createStore(reducers);
 
 ReactDOM.render(
