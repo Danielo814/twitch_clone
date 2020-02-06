db3825fa6298691a4f36fe4503f31abb7a75902e
Danielo814 <ojedadaniel92@gmail.com>
2020-02-05 18:39:14 -0800

    add meta parameter to extract error message

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  renderInput({ input, label }) {
+  renderInput({ input, label, meta }) {
     return (
       <div className="field">
         <label>{label}</label>
