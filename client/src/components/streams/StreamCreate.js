06d1fb4a8ae0066e7ca923708a6f281e80cd2cf9
Danielo814 <ojedadaniel92@gmail.com>
2020-02-05 18:53:24 -0800

    pass meta property to renderError function

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -7,7 +7,7 @@ class StreamCreate extends React.Component {
       <div className="field">
         <label>{label}</label>
         <input {...input} autoComplete="off" />
-        {this.renderError}
+        {this.renderError(meta)}
         <div>{meta.error}</div>
       </div>
     );
