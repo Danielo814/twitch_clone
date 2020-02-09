d04741b1407c38756481d09e68c7971d7ea656ce
Danielo814 <ojedadaniel92@gmail.com>
2020-02-08 17:04:20 -0800

    delete a stream using lodash

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -5,6 +5,7 @@ import {
   EDIT_STREAM,
   DELETE_STREAM
 } from "../actions/types";
+import _ from "lodash";
 
 export default (state = {}, action) => {
   switch (action.type) {
@@ -12,6 +13,10 @@ export default (state = {}, action) => {
       return { ...state, [action.payload.id]: action.payload };
     case CREATE_STREAM:
       return { ...state, [action.payload.id]: action.payload };
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case DELETE_STREAM:
+      return _.omit(state.action.payload);
     default:
       return state;
   }
