a4c3bb487f6dcbf9d6926aa33ff4714418d5f19b
Danielo814 <ojedadaniel92@gmail.com>
2020-02-09 14:35:14 -0800

    call component in render method

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -22,8 +22,12 @@ class StreamList extends React.Component {
   }
 
   render() {
-    console.log(this.props.streams);
-    return <div></div>;
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">{this.renderList()}</div>
+      </div>
+    );
   }
 }
 
