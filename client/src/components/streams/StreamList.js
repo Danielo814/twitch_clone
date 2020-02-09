ae789da2f79ab7ebe355293553d71c589404e1e6
Danielo814 <ojedadaniel92@gmail.com>
2020-02-09 14:32:11 -0800

    render title to screen

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -12,6 +12,7 @@ class StreamList extends React.Component {
       return (
         <div className="item" key={stream.id}>
           <i className="large middle aligned icon camera" />
+          <div className="content">{stream.title}</div>
         </div>
       );
     });
