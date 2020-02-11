496b88f79d996ccb529a86c24c19f094cd02d538
Danielo814 <ojedadaniel92@gmail.com>
2020-02-11 15:00:55 -0800

    print title

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -11,7 +11,7 @@ class StreamEdit extends React.Component {
     if (!this.props.stream) {
       return <div>Loading...</div>;
     }
-    return <div>StreamEdit</div>;
+    return <div>{this.props.stream.title}</div>;
   }
 }
 
