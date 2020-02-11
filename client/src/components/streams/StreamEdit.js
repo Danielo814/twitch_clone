a655b31f2fa60c300fad52cf7a96a6bc99807487
Danielo814 <ojedadaniel92@gmail.com>
2020-02-11 14:48:06 -0800

    import fetch stream

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
 const StreamEdit = props => {
   return <div>StreamEdit</div>;
@@ -9,4 +10,4 @@ const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps)(StreamEdit);
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
