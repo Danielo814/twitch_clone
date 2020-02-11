ee0588848a0a75a291fafb4cbe7fe0271d99796b
Danielo814 <ojedadaniel92@gmail.com>
2020-02-11 15:24:08 -0800

    use onSubmit prop instead of function

--- client/src/components/streams/StreamForm.js
+++ client/src/components/streams/StreamForm.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
-import { connect } from "react-redux";
-import { createStream } from "../../actions";
 
 class StreamForm extends React.Component {
   renderError({ error, touched }) {
@@ -26,7 +24,7 @@ class StreamForm extends React.Component {
   };
 
   onSubmit = formValues => {
-    this.props.createStream(formValues);
+    this.props.onSubmit(formValues);
   };
 
   render() {
@@ -60,6 +58,6 @@ const validate = formValues => {
 };
 
 export default reduxForm({
-  form: "streamCreate",
+  form: "streamForm",
   validate
 })(StreamForm);
