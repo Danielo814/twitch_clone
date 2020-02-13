23c61752bb5fc92f8f7090b5bf15d7896bc6d510
Danielo814 <ojedadaniel92@gmail.com>
2020-02-13 14:43:40 -0800

    use reactdom createportal and render modal

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Modal = props => {};
+const Modal = props => {
+  return ReactDOM.createPortal(
+    <div className="ui dimmer modals visible active"></div>
+  );
+};
 
 export default Modal;
