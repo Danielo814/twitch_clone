e6084b52c7bbf22b795c80459cc0f0c5f2597ccf
Danielo814 <ojedadaniel92@gmail.com>
2020-02-15 15:31:24 -0800

    add constructor method

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -4,6 +4,10 @@ import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
@@ -15,6 +19,7 @@ class StreamShow extends React.Component {
 
     return (
       <div>
+        <video />
         <h1>{this.props.stream.title}</h1>
         <h5>{this.props.stream.description}</h5>
       </div>
