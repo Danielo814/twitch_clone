ab5a4c407085a6e991db6264e0a676f38b8f5bff
Danielo814 <ojedadaniel92@gmail.com>
2020-02-15 15:35:44 -0800

    expand video element

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -20,7 +20,7 @@ class StreamShow extends React.Component {
 
     return (
       <div>
-        <video />
+        <video ref={this.videoRef} style={{ width: "100%" }} />
         <h1>{this.props.stream.title}</h1>
         <h5>{this.props.stream.description}</h5>
       </div>
